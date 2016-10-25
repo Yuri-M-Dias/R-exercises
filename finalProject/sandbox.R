65589f390ee8b186d58199e8b68d95c64590194b
Yuri-M-Dias <yurimathe.yp@gmail.com>
2016-10-24 23:09:08 -0200

    Adicionado exemplo que consegue ler o caminho do arquivo de dados, e classificar ele de uma forma.

--- finalProject/sandbox.R
+++ finalProject/sandbox.R
@@ -1,6 +1,10 @@
 library("rjson")
 
-json_data <- fromJSON(file="R-exercises/finalProject/metoriteDate.json")
-summary(json_data)
-sapply(json_data, class)
-plot(json_data)
+filePath <- paste(getwd(), "/metoriteDate.json", sep = "")
+
+json_data_raw <- fromJSON(file = filePath)
+meteorites <- json_data_raw[["data"]]
+meteoritesCitiesNames <- sapply(meteorites, function(x) x[[9]])
+
+# To look for later
+# http://zevross.com/blog/2015/02/12/using-r-to-download-and-parse-json-an-example-using-data-from-an-open-data-portal/
