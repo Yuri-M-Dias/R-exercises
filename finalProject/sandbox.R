5746c06a2b1edc069ae55786defb937c598b1231
Yuri-M-Dias <yurimathe.yp@gmail.com>
2016-11-15 21:25:30 -0200

    Reformat the main file for legibility
    Add a possibility in using the data with the correct names

--- finalProject/sandbox.R
+++ finalProject/sandbox.R
@@ -1,32 +1,64 @@
 # Sandbox for testing the project, with various helpful links
-# Data comes from https://data.nasa.gov/Space-Science/Meteorite-Landings/gh4g-9sfh
-# Tutorial on reading from an Open Data Portal
+# Data comes from: https://data.nasa.gov/Space-Science/Meteorite-Landings/gh4g-9sfh
+# Tutorial on reading from an Open Data Portal:
 # http://zevross.com/blog/2015/02/12/using-r-to-download-and-parse-json-an-example-using-data-from-an-open-data-portal/
 
-# Dependencies (should be in Packrat? What about build and packaging tools?)
+# Dependencies
 library("rjson")
 library("gdata")
 
-# Reads data
-filePath <- paste(getwd(), "/metoriteDate.json", sep = "")
-json_data_raw <- fromJSON(file = filePath)
+# Constants
+# Probably better as an argument?
+DATA_FILE_PATH <- 'metoriteDate.json'
+# Just testing out how constants work
+#lockBinding("DATA_FILE_PATH", globalenv())
 
-meteorites <- json_data_raw[["data"]]
-
-returnData<-function(x, var){
-  if(is.null(x[[var]])){
+#Functions
+readDimension <- function(element, position){
+  if(is.null(element[[position]])){
     return(NA)
   }
-  return(trim(x[[var]]))
+  return(trim(element[[position]]))
 }
 
-grabInfo <- function(var){
-  print(paste("Variable", var, sep=" "))  
-  sapply(meteorites, function(x) returnData(x, var)) 
+readInfoFromSingleVariable <- function(position, data) {
+  print(paste("Variable", position, sep=" "))
+  sapply(data, function(element) readDimension(element, position))
 }
+
+readData <- function(data, metaData) {
+  # Ignores GeoLocation for now
+  numberOfColumns <- length(metaData[["view"]][["columns"]]) - 1
+  dataFrame <- data.frame(
+    sapply(1:numberOfColumns,
+           function(columnIndex){ readInfoFromSingleVariable(columnIndex, data) }
+           ),
+    stringsAsFactors=FALSE
+    )
+  return(dataFrame)
+}
+
+getColumnName <- function(columns, elementIndex){
+  subColumnTypes <- columns[[elementIndex]]$subColumnTypes
+  if(is.null(subColumnTypes)){
+    return(columns[[elementIndex]]$name)
+  }else{
+    return(subColumnTypes)
+  }
+}
+
+# Reads data
+rawJsonData <- fromJSON(file = DATA_FILE_PATH)
+
+metaData <- rawJsonData[['meta']]
+meteoritesData <- rawJsonData[['data']]
+dataColumns <- metaData[['view']][['columns']]
 
 # do the extraction and assembly
-fmDataDF<-data.frame(sapply(1:17, grabInfo), stringsAsFactors=FALSE)
-
-meteoritesCitiesNames <- sapply(meteorites, function(x) x[[9]])
+dataColumnsNames <- unlist(
+  sapply(1:length(dataColumns),
+         function(element) getColumnName(dataColumns, element))
+)
+formatedData <- readData(meteoritesData, metaData)
+names(formatedData) <- dataColumnsNames
 
