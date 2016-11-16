eee8fcded9a879f93fc39fdadcff5fd84ecd0ad9
Yuri-M-Dias <yurimathe.yp@gmail.com>
2016-11-15 22:40:22 -0200

    Corrected repository structure
    Added basic shapefile of the world
    Corrected some functions for correct output
    Added a simple visualization

--- finalProject/sandbox.R
+++ finalProject/sandbox.R
@@ -4,47 +4,49 @@
 # http://zevross.com/blog/2015/02/12/using-r-to-download-and-parse-json-an-example-using-data-from-an-open-data-portal/
 
 # Dependencies
+# Reads json
 library("rjson")
+# Trim function(removes spaces)
 library("gdata")
+# Some more read options
+#library("rgdal")
+# better graphics, supposedly
+#library("ggplot2")
 
 # Constants
 # Probably better as an argument?
-DATA_FILE_PATH <- 'metoriteDate.json'
+DATA_FILE_PATH <- 'data/metoriteDate.json'
 # Just testing out how constants work
 #lockBinding("DATA_FILE_PATH", globalenv())
 
 #Functions
-readDimension <- function(element, position){
-  if(is.null(element[[position]])){
+readDimension <- function(element, position) {
+  if (is.null(element[[position]])) {
     return(NA)
   }
   return(trim(element[[position]]))
 }
 
-readInfoFromSingleVariable <- function(position, data) {
-  print(paste("Variable", position, sep=" "))
-  sapply(data, function(element) readDimension(element, position))
-}
-
-readData <- function(data, metaData) {
-  # Ignores GeoLocation for now
-  numberOfColumns <- length(metaData[["view"]][["columns"]]) - 1
-  dataFrame <- data.frame(
-    sapply(1:numberOfColumns,
-           function(columnIndex){ readInfoFromSingleVariable(columnIndex, data) }
-           ),
-    stringsAsFactors=FALSE
-    )
+readData <- function(data, metaData, dataColumnsNames) {
+  numberOfColumns <- length(dataColumnsNames)
+  dataFrame <- data.frame(sapply(1:numberOfColumns,
+                                 function(columnIndex) {
+                                   print(paste("Variable:", dataColumnsNames[columnIndex] , sep = " "))
+                                   sapply(data, function(element)
+                                     readDimension(element, columnIndex))
+                                 }),
+                          stringsAsFactors = FALSE)
+  names(dataFrame) <- dataColumnsNames
+  # TODO: make this another function, just to separate these transformations
+  # Formats latitude and longitude
+  dataFrame$reclat <- as.numeric(dataFrame$reclat)
+  dataFrame$reclong <- as.numeric(dataFrame$reclong)
+  dataFrame$'mass (g)' <- as.numeric(dataFrame$'mass (g)')
   return(dataFrame)
 }
 
-getColumnName <- function(columns, elementIndex){
-  subColumnTypes <- columns[[elementIndex]]$subColumnTypes
-  if(is.null(subColumnTypes)){
-    return(columns[[elementIndex]]$name)
-  }else{
-    return(subColumnTypes)
-  }
+getColumnName <- function(columns, elementIndex) {
+  return(columns[[elementIndex]]$name)
 }
 
 # Reads data
@@ -53,12 +55,14 @@ rawJsonData <- fromJSON(file = DATA_FILE_PATH)
 metaData <- rawJsonData[['meta']]
 meteoritesData <- rawJsonData[['data']]
 dataColumns <- metaData[['view']][['columns']]
+# Ignores GeoLocation for now
+numberOfColumns <- length(dataColumns) - 1
 
 # do the extraction and assembly
-dataColumnsNames <- unlist(
-  sapply(1:length(dataColumns),
-         function(element) getColumnName(dataColumns, element))
-)
-formatedData <- readData(meteoritesData, metaData)
-names(formatedData) <- dataColumnsNames
+dataColumnsNames <- unlist(sapply(1:numberOfColumns,
+                                  function(element) {
+                                    return(dataColumns[[element]]$name)
+                                  }))
+formatedData <- readData(meteoritesData, metaData, dataColumnsNames)
 
+barplot(formatedData$`mass (g)`, beside = TRUE)
